fb2195eabf75a2b465d58083f35840232b301492
Vaibhav Chavan <vaibhavchvn86@gmail.com>
2021-09-19 14:27:49 +0530

    Solution file

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Day6Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // FibonacciNum.Fibnum();
+            //PerfectNum.Num();
+            PrimeNum.Prime();
+            //ReverseNum.Reverse();
+            //CouponNum.Coupon();
+           // Stopwat.Watch();
+        }
+    }
+}
