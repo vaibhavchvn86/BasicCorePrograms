48db25756cf5cad554b466f97590d85b1abea2e7
Vaibhav Chavan <vaibhavchvn86@gmail.com>
2021-09-19 14:26:01 +0530

    Reverse Number

---
+++ ReverseNum.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day6Assignment
+{
+    class ReverseNum
+    {
+        public static void Reverse()
+        {
+            Console.WriteLine("Enter the Number to Reverse");
+            int N = Convert.ToInt32(Console.ReadLine());
+           
+            int Rev = 0;
+            while (N!=0)
+            {
+               int R = N % 10;
+                
+                Rev = (Rev * 10 )+ R;
+                N /= 10;
+            }
+            Console.WriteLine(+Rev);
+
+        }
+    }
+}
