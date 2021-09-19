90eccd3946bfd1b63c156626c7799a017fbe7a46
Vaibhav Chavan <vaibhavchvn86@gmail.com>
2021-09-19 14:24:47 +0530

    Fibonacci Number series

---
+++ FibonacciNum.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day6Assignment
+{
+    class FibonacciNum
+    {
+        public static void Fibnum()
+        {
+            int b=1;
+            int a=0;
+            int N;
+            int c=0;
+            int i;
+            int d = 0;
+
+            Console.WriteLine("Enter the Number to find Fibonacci Series till N");
+            N = Convert.ToInt32(Console.ReadLine());
+            
+            Console.WriteLine("Fibnum " + d);
+            for (i = 2; i <= N; i++)
+            {
+                c = a + b;
+                Console.WriteLine("Fibnum " + c);
+                a = b;
+                b = c;
+            }
+        }
+        }
+    }
+
