67df4b4ba5922a6d5e47a93697388358df57a64d
Vaibhav Chavan <vaibhavchvn86@gmail.com>
2021-09-19 14:23:46 +0530

    Perfect Number

---
+++ PerfectNum.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day6Assignment
+{
+    class PerfectNum
+    {
+        public static void Num()
+        { 
+            Console.WriteLine("Enter the Number N");
+            int N = Convert.ToInt32(Console.ReadLine());
+            int sum = 0;
+            for (int i = 1; i < N; i++)
+            {
+                if (N % i == 0)
+                {
+                    sum = sum + i;
+                    Console.WriteLine("Divisor of N = " +i);
+                }
+            }
+            if (sum == N)
+            {
+                Console.WriteLine("Entered number "+N +" is a perfect number");  
+            }
+            else
+            {
+                Console.WriteLine("Entered number "+N+" is not a perfect number");
+            }
+        }
+    }
+    
+}
