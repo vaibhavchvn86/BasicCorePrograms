748a7416685cc3deea6c23ee2774bedf77e1ffea
Vaibhav Chavan <vaibhavchvn86@gmail.com>
2021-09-19 14:26:52 +0530

    Stopwatch Program

---
+++ Stopwat.cs
@@ -0,0 +1,21 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Text;
+using System.Diagnostics;
+using System.Threading;
+
+namespace Day6Assignment
+{
+    class Stopwat
+    {
+        public static void Watch()
+        {
+            Stopwatch StopW = new Stopwatch();
+            StopW.Start();
+            Thread.Sleep(1000);
+            StopW.Stop();
+            TimeSpan T = StopW.Elapsed;
+            Console.WriteLine("Stopwatch Runtime= " + T);
+        }
+    }
+}
