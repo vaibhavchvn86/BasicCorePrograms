3e9dfdd478137e223ca27e99dcbb03d408c2cde4
Vaibhav Chavan <vaibhavchvn86@gmail.com>
2021-09-20 20:19:50 +0530

    Unique Coupon Number

---
+++ CouponNum.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Day6Assignment
+{
+    class CouponNum
+    {
+        static char[] Num = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz".ToCharArray();
+        public static void Coupon()
+        {
+            Console.WriteLine("Enter N number of coupon: ");
+            int N = Convert.ToInt32(Console.ReadLine());
+                int CLength = 6;
+                Random random = new Random();
+                for (int CNum = 1; CNum <= N; CNum++)
+                {
+                    StringBuilder STRING = new StringBuilder();
+                    for (int i = 0; i < CLength; i++)
+                    {
+                        int position = random.Next(0, Num.Length);
+                        STRING.Append(Num[position]);
+                    }
+                    Console.WriteLine("Unique Coupon Number - {0}:  {1}", CNum, STRING);
+                }
+        }
+    }
+}
